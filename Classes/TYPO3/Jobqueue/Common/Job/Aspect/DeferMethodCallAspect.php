2f0bf5da1da529dd36ddeb485e6c880e2c9dfadc
Karsten Dambekalns <karsten@dambekalns.de>
2015-10-22 17:08:54 +0200

    [TASK] Clean up package a bit
    - This replaces the old file level docblocks and adds the MIT license to
      the package.
    - The composer manifest is tweaked a bit.
    - A code of conduct is added.
    - Some tiny code tweaks are done.

--- Classes/TYPO3/Jobqueue/Common/Job/Aspect/DeferMethodCallAspect.php
+++ Classes/TYPO3/Jobqueue/Common/Job/Aspect/DeferMethodCallAspect.php
@@ -1,19 +1,20 @@
 <?php
 namespace TYPO3\Jobqueue\Common\Job\Aspect;
 
-/*                                                                        *
- * This script belongs to the TYPO3 Flow package "TYPO3.Jobqueue.Common". *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU General Public License, either version 3 of the   *
- * License, or (at your option) any later version.                        *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+/*
+ * This file is part of the TYPO3.Jobqueue.Common package.
+ *
+ * (c) Contributors to the package
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
 
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Aop\JoinPointInterface;
 use TYPO3\Flow\Reflection\ReflectionService;
+use TYPO3\Jobqueue\Common\Annotations\Defer;
 use TYPO3\Jobqueue\Common\Job\JobManager;
 use TYPO3\Jobqueue\Common\Job\StaticMethodCallJob;
 
@@ -52,7 +53,7 @@ class DeferMethodCallAspect
         if ($this->processingJob) {
             return $joinPoint->getAdviceChain()->proceed($joinPoint);
         }
-        $deferAnnotation = $this->reflectionService->getMethodAnnotation($joinPoint->getClassName(), $joinPoint->getMethodName(), 'TYPO3\Jobqueue\Common\Annotations\Defer');
+        $deferAnnotation = $this->reflectionService->getMethodAnnotation($joinPoint->getClassName(), $joinPoint->getMethodName(), Defer::class);
         $queueName = $deferAnnotation->queueName;
         $job = new StaticMethodCallJob($joinPoint->getClassName(), $joinPoint->getMethodName(), $joinPoint->getMethodArguments());
         $this->jobManager->queue($queueName, $job);
--- Classes/TYPO3/Jobqueue/Common/Job/JobInterface.php
+++ Classes/TYPO3/Jobqueue/Common/Job/JobInterface.php
@@ -1,15 +1,15 @@
 <?php
 namespace TYPO3\Jobqueue\Common\Job;
 
-/*                                                                        *
- * This script belongs to the TYPO3 Flow package "TYPO3.Jobqueue.Common". *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU General Public License, either version 3 of the   *
- * License, or (at your option) any later version.                        *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+/*
+ * This file is part of the TYPO3.Jobqueue.Common package.
+ *
+ * (c) Contributors to the package
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
 
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Jobqueue\Common\Queue\Message;
